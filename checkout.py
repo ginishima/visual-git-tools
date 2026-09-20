256077dacddb9f70027e2a584a393e5c2055e3d6
Ginishima <giniszyma@gmail.com>
2026-09-19 21:23:34 -0400

    Update checkout.py
    Added a new total counter to checkout.py

--- checkout.py
+++ checkout.py
@@ -8,5 +8,6 @@ celery = 1.29
 tax = (apple + banana + strawberries + celery) * .06
 
 total = tax + (apple + banana + strawberries + celery)
+total = 0
 
 print(f"Your total for today is ${total:.2f}, including tax.")
