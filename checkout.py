fd837a534bfaf31bcd377881ae0c7837e89e9a98
Ginishima <giniszyma@gmail.com>
2026-09-19 21:48:23 -0400

    Update checkout.py
    This change changes some lines in checkout.py to reflect that the 2.00 added previously is not some bogus charge, it is a convenience fee.

--- checkout.py
+++ checkout.py
@@ -5,9 +5,10 @@ banana = .89
 strawberries = 3.49
 celery = 1.29
 
+convenience_fee = 2.00
+
 tax = (apple + banana + strawberries + celery) * .06
 
-total = tax + (apple + banana + strawberries + celery)
-total = total + 2.00
+total = tax + convenience_fee + (apple + banana + strawberries + celery)
 
 print(f"Your total for today is ${total:.2f}, including tax.")
